47e13364ffe127469a5c0255425d82a3ef8342eb
smardhekar <75525220+smardhekar@users.noreply.github.com>
2022-11-01 23:16:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstates"
-    storage_account_name = "kstfstateaccount"
+    storage_account_name = "kstfstateaccount1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
